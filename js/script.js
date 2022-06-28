7a0fef4799c61d3c835f943284eb536f95fa19b9
amaljom <amaljom99@gmail.com>
2022-06-28 15:50:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const workerInfo= 
+[
+    // ? primo
+    {
+      name: 'Wayne Barnett',
+      role:'Founder & CEO',
+      photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    // ? secondo
+    {
+        name: 'Angela Caroll',
+        role:'Chief Editor',
+        photo:' angela-caroll-chief-editor.jpg'
+    },
+    // ? terzo
+    {
+    name: 'Walter Gordon',
+    role:'Office Manager',
+    photo:'walter-gordon-office-manager.jpg'
+    },
+    // ? quarto
+    {
+        name:'Angela Lopez',
+        role:'Social Media Manager',
+        photo:'angela-lopez-social-media-manager.jpg'
+    },
+     // ? quinto
+    {
+        name: 'Scott Estrada',
+        role:'Developer',
+        photo:' scott-estrada-developer.jpg'
+    },
+    // ? sesto
+    {
+    name: 'Barbara Ramos',
+    role:'Graphic Designer',
+    photo:'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+console.log(workerInfo);
